6171435f7af2a18d02e56866fdd9549e4dfce975
Peter <plowry812@gmail.com>
2013-06-29 21:37:09 -0700

    initialized site with design concept

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lowdown::Application.config.secret_token = 'e42268d4164d3fd28abd1746997b00dc90a600916e07c1a105901b8912fe541550ed259dcdeafc33e4a486b59c25e9be8a2d06fbfeaa3d2bd69427a4f737005c'
